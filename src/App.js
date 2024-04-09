52a44aa47672b48bf945a0b8310c6183a017e0f4
Khang NT <vn.khangnt@gmail.com>
2024-04-09 23:10:15 +0700

    fix css mess

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Provider from "lib/Provider";
 import api from "lib/api";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import "App.css";
+import "tailwind.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 
 api.start();
 
